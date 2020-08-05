96d587d20157eb4f977863a50fb88389fb7c18c7
omar <dinasil.omarbek@gmail.com>
2020-08-06 02:37:26 +0600

    edited transactional; changed root password.

--- src/main/java/kz/academy/kemelacademy/controllers/UserController.java
+++ src/main/java/kz/academy/kemelacademy/controllers/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -38,6 +39,7 @@ public class UserController {
     @Autowired
     private MessageSource messageSource;
     
+    @Transactional
     @PostMapping
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetailsRequestModel) {
         UserRest returnValue = new UserRest();
