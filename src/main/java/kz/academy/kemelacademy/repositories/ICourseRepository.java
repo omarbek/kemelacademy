1113bb8f66f27882b8fa7d188ec02152c8c7bfd2
omar <dinasil.omarbek@gmail.com>
2020-08-09 14:46:35 +0600

    edited get category by id with deleted field

--- src/main/java/kz/academy/kemelacademy/repositories/ICourseRepository.java
+++ src/main/java/kz/academy/kemelacademy/repositories/ICourseRepository.java
@@ -2,6 +2,9 @@ package kz.academy.kemelacademy.repositories;
 
 import kz.academy.kemelacademy.ui.entity.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 /**
  * @author Omarbek.Dinassil
@@ -9,4 +12,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @project kemelacademy
  */
 public interface ICourseRepository extends JpaRepository<CourseEntity, Long> {
+    
+    @Override
+    @Query("select c from CourseEntity c where c.deleted = false and id = :id")
+    Optional<CourseEntity> findById(Long id);
+    
 }
