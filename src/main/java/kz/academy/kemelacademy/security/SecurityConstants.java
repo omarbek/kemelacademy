0c6dcddf8b4487716676b5e3631f4e9b18669143
omar <dinasil.omarbek@gmail.com>
2020-08-28 10:54:53 +0600

    added implicit to level controller

--- src/main/java/kz/academy/kemelacademy/security/SecurityConstants.java
+++ src/main/java/kz/academy/kemelacademy/security/SecurityConstants.java
@@ -31,6 +31,7 @@ public class SecurityConstants {
     static final String FILE_TYPES_URL = "/file_types/**";
     static final String LANGUAGES_URL = "/languages/**";
     static final String LESSON_TYPES_URL = "/lesson_types/**";
+    static final String LEVELS_URL = "/levels/**";
     
     public static String getTokenSecret() {
         AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
